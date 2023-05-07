3c021cdd6b90fc6892a290dc1ffd35ddab6ae70c
hduale13 <hodo.duale1@marist.edu>
2023-05-07 15:07:03 -0400

    commit all

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+
+  <title>Document</title>
+
+</head>
+
+<body>
+  <h1>List of Members </h2>
+  <table>
+    <thead>
+      <tr>
+        <th>mem_id</th>
+        <th>name</th>
+        <th>phone</th>
+
+      </tr>
+    </thead>
+  </table>
+
+</body>
+</html>
